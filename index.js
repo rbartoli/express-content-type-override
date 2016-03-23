9643fb8c2ae3807a7bd4530d72530124184d5b8e
Riccardo Bartoli <rbartoli@salmon.com>
2016-03-23 14:00:12 +0000

    Improve formatting and remove unused comments

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
-/**
- *
- * @package     express-content-type-override
- * @category    Express Middleware
- * @version     1.0.0
- * @copyright   Copyright (c) 2015. All rights reserved.
- * @license     MIT
- * @author      Riccardo Bartoli <info@rblab.com>
- *
- */
-
-module.exports = function ( options ) {
+module.exports = function (options) {
     var contentType;
     options = options || {};
-    contentType = options.contentType || 'application/x-www-form-urlencoded';
+    options.contentType = options.contentType || 'application/x-www-form-urlencoded';
+    
     if (options.charset) {
-        contentType += ';charset=' + options.charset;
+        options.contentType += ';charset=' + options.charset;
     }
 
-    return function( req, res, next ) {
-        if ( !req.headers['content-type'] || !req.headers['content-type'].match( contentType ) ) {
-            // Set the content-type header manually
-            req.headers['content-type'] = contentType;
+    return function(req, res, next) {
+        if (!req.headers['content-type'] || !req.headers['content-type'].match(options.contentType)) {
+            req.headers['content-type'] = options.contentType;
         }
 
         next();
